9b0a9c04b3f0559a4637649677c2abd6526b8ce2
Anton <suddyadred@gmail.com>
2017-03-18 19:39:48 +0200

    Initial commit

---
+++ assets/js/custom/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    //for hover effect
+    $( ".level-list" ).hover(
+        function() {
+            $( this ).addClass( "hover" );
+        }, function() {
+            $( this ).removeClass( "hover" );
+        }
+    );
+
+});

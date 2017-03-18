76e5a4915a13f6301fca846e13e262baa6e223c0
Anton <suddyadred@gmail.com>
2017-03-18 20:05:06 +0200

    suddya

--- assets/js/custom/scripts.js
+++ assets/js/custom/scripts.js
@@ -1,3 +1,13 @@
+function setMaxHeight(box) {
+    var maxHeight = 0;
+    box.each(function () {
+        if ($(this).height() > maxHeight) {
+            maxHeight = $(this).height();
+        }
+    });
+    box.height(maxHeight);
+}
+
 $(document).ready(function() {
     //for hover effect
     $( ".level-list" ).hover(
@@ -8,4 +18,26 @@ $(document).ready(function() {
         }
     );
 
+    //for section level 1
+    $(window).on('load resize', function() {
+        var width = $(window).width(),
+            windowHeight =  $(window).height(),
+            listHeight = $('.level-list').height(),
+            listBox = $('.level-list li'),
+            contentBox = listBox.find('.content-wrap');
+
+        if (width > '1024') {
+            setTimeout(function () {
+                setMaxHeight(listBox);
+
+                if (listHeight > windowHeight) {
+                    setMaxHeight(contentBox);
+                } else {
+                    listBox.height(windowHeight);
+                }
+            }, 100);
+        } else {
+
+        }
+    });
 });

4f0ecda9eae9b06af277b27d548abbd7d264bce9
Anton <suddyadred@gmail.com>
2017-03-22 17:03:15 +0200

    suddya

--- assets/js/custom/scripts.js
+++ assets/js/custom/scripts.js
@@ -132,11 +132,4 @@ jQuery(document).ready(function($) {
         });
     }
 
-    //for popup
-    $('.open-popup-link').magnificPopup({
-        type:'inline',
-        midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-        removalDelay: 300,
-        mainClass: 'mfp-fade'
-    });
 });
